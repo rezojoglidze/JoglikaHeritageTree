6dbb1ff1d4fb05063f942a85070d030a5bec6393
Rezo Joglidze <rezojoglidze7@gmail.com>
2021-05-03 22:16:53 +0400

    first commit

---
+++ src/main/java/com/joglikaheritagetree/JoglikaheritagetreeApplication.java
@@ -0,0 +1,13 @@
+package com.joglikaheritagetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoglikaheritagetreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoglikaheritagetreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joglikaheritagetree/JoglikaheritagetreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joglikaheritagetree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoglikaheritagetreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
